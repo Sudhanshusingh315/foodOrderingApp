f7723e5b2b5ac9816e0ccd6a7a391d055fdfaf95
LeaderOfMeow <ss826063@gmail.com>
2024-05-04 23:04:53 +0530

    starting with backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Cart from "./pages/Cart";
+import ProceedToCheckOut from "./pages/PTCO";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Index />}>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<ProceedToCheckOut />} />
         </Route>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
---
+++ frontend/src/pages/PTCO.jsx
@@ -0,0 +1,8 @@
+export default function ProceedToCheckOut(){
+    return (
+        <div className="container h-screen sm:grid sm:grid-cols-5">
+            <div className="w-20 h-20">hello</div>
+            <div className="w-20 h-20">hello</div>
+        </div>
+    )
+}
